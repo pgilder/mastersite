639ca36ba167afda7952e01ffbfcb7b3ca6f6dcb
Patrick Gilder <pgilder@cyberitas.com>
2018-11-13 14:59:18 -0700

    Updates

--- wp-content/themes/wildanimal/template-parts/header.php
+++ wp-content/themes/wildanimal/template-parts/header.php
@@ -5,12 +5,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 <header id="site-header" class="site-header" role="banner">
 
-	<div id="logo">
+	<div id="logo" style="max-width: 130px;height: auto;padding: 0px 0px 20px 20px;">
 		<?php the_custom_logo(); ?>
 	</div>
-
-	<nav id="top-menu" role="navigation">
-		<?php // top menu ?>
-	</nav>
 
 </header>
